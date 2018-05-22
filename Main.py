6062f9984153ea0110fd7ef0f1f55200e10183bf
ben-mackenzie <30845590+ben-mackenzie@users.noreply.github.com>
2018-05-21 22:17:05 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,37 +1,3 @@
-'''
-1) Ben Mackenzie
-2) 3/19/2018
-3) Program specifications
-
-I. Purpose of the program and any information pertaining to its description
-This project is designed to explore hashing in general and specifically
-three methods for resolving collisions in hash tables:
-linear probing, quadratic probing, and double hash probing.
-
-This has been done by creating 3 classes for these probing techniques
-using the LinearProbingHash class as a base class and a nested Record
-class within LinearProbingHash for tracking the <key, value> pairs 
-being inserted into the hash tables.
-
-A driver is included to parse 4 text files, test the hash class methods
-on the data contained within the parsed lists, and track the collisions
-that occur when records are inserted into hash tables.
-
-II. Input
-Driver inputs:
-A text file containing numbers.
-The name of an output file to write results to.
-
-III. Output
-Data on collisions, record keys, and record values is written to
-the output file listed in the driver call.
-
-IV. Bugs or Implemented Test Cases; and, any theoretical follow-up including assignment questions to be turned in for grading
-Linear probing, quadratic probing, and double hash probing methods
-are tested on lists containing random increasing, random decreasing,
-and just plain random numbers.
-'''
-
 from LinearProbingHash import LinearProbingHash
 from QuadraticProbingHash import QuadraticProbingHash
 from DoubleHashingProbingHash import DoubleHashingProbingHash
